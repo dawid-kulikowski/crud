78279757667e0edd4e314905967ad915c0475e45
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-06-16 12:43:20 +0200

    First CRUD Project - UPDATE and DELETE setup

--- src/main/java/com/crud/app/crud/Controller/ApiControllers.java
+++ src/main/java/com/crud/app/crud/Controller/ApiControllers.java
@@ -3,10 +3,7 @@ package com.crud.app.crud.Controller;
 import com.crud.app.crud.Models.User;
 import com.crud.app.crud.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,18 +14,40 @@ public class ApiControllers {
     private UserRepo userRepo;
 
     @GetMapping(value = "/")
-    public String getPage(){
+    public String getPage() {
         return "Welcome";
     }
+
     @GetMapping(value = "/users")
     public List<User> getUsers() {
         return userRepo.findAll();
     }
 
-    @PostMapping (value = "/save")
+    @PostMapping(value = "/save")
     public String saveUser(@RequestBody User user) {
         userRepo.save(user);
         return "Saved...";
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user) {
+        User updatedUser = userRepo.findById(id).get();
+        updatedUser.setFirstName(user.getFirstName());
+        updatedUser.setLastName(user.getLastName());
+        updatedUser.setAge(user.getAge());
+        updatedUser.setOccupation(user.getOccupation());
+
+        userRepo.save(updatedUser);
+        return "Updated...";
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteUser(@PathVariable long id) {
+        User deleteUser = userRepo.findById(id).get();
+        userRepo.delete(deleteUser);
+        return "Deleted user with id:" + id;
+    }
 
     }
-}
+
+
